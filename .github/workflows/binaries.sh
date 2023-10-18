46de9691a66cefa8a40788dbe5d7083f3c670da0
David Leal <halfpacho@gmail.com>
2023-10-18 09:54:27 -0600

    Create binaries on release

---
+++ .github/workflows/binaries.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Packs up the created binaries by the release workflow
+# which are then uploaded as release assets in any release.
+
+mkdir -p binaries
+7z a -mx=9 -tzip memory_game-x64.zip vc_redistributable_links.txt Memory_Game/bin/x64/Release/Memory_Game.exe
+7z a -mx=9 -ttar memory_game-x64.tar vc_redistributable_links.txt Memory_Game/bin/x64/Release/Memory_Game.exe
+7z a -mx=9 -tzip memory_game-x86.zip vc_redistributable_links.txt Memory_Game/bin/x86/Release/Memory_Game.exe
+7z a -mx=9 -ttar memory_game-x86.tar vc_redistributable_links.txt Memory_Game/bin/x86/Release/Memory_Game.exe
+
+# Use the GZip compression algorithm
+7z a -mx=9 -tgzip memory_game-x64.tar.gz memory_game-x64.tar
+7z a -mx=9 -tgzip memory_game-x86.tar.gz memory_game-x86.tar
+
+# We're going to use TAR.GZ files
+rm memory_game-x64.tar
+rm memory_game-x86.tar
+
+mv memory_game-x64.zip binaries/
+mv memory_game-x64.tar.gz binaries/
+mv memory_game-x86.zip binaries/
+mv memory_game-x86.tar.gz binaries/
